6515686f99d0bb9c88447a3ad3c638560b892dd9
chenci95 <77560646+chenci95@users.noreply.github.com>
2021-01-19 17:53:00 +0800

    1

---
+++ list.js
@@ -0,0 +1,69 @@
+jsonhacknet(
+{
+	"name": "\/",
+	"children": [{
+		"name": "服务器工具合集",
+		"children": [
+		{
+		"name": "宝塔",
+			"down": "https://download.bt.cn/win/panel/BtSoft.zip",
+			"size": 3072327,
+			"time": 1611048433,
+		"description": "宝塔Windows面板，支持一键建站、FTP、数据库，以及管理服务器"
+		},
+		{
+			"name": "phpStudy最新版",
+			"down": "http://public.xp.cn/upgrades/phpStudy_64.zip",
+			"size": 81788928,
+			"time": 1611048433,
+			"description": "小皮面板(phpstudy) - 让天下没有难配的服务器环境！"
+		},
+		{
+			"name": "phpStudy 2018",
+			"down": "http://public.xp.cn/upgrades/PhpStudy2018.zip",
+			"size": 63963136,
+			"time": 1611048433,
+			"description": "小皮面板(phpstudy) - 让天下没有难配的服务器环境！"
+		},
+		{
+			"name": "phpStudy 2014",
+			"down": "https://so.hacksafe.net/down/phpStudy_2014.zip",
+			"size": 37748736,
+			"time": 1611048433,
+			"description": "小皮面板(phpstudy) - 让天下没有难配的服务器环境！"
+		},
+		{
+			"name": "WinRAR 压缩包",
+			"down": "https://so.hacksafe.net/down/WinRAR.exe",
+			"size": 3460300,
+			"time": 1611048433
+		},
+		{
+			"name": "VC++环境合集",
+			"down": "https://so.hacksafe.net/down/msvbcrt.zip",
+			"size": 36700160,
+			"time": 1611048433
+		},
+		{
+			"name": "Navicat for MySQL",
+			"down": "https://so.hacksafe.net/down/NavicatforMySQL.zip",
+			"size": 22020096,
+			"time": 1611048433
+		},
+		{
+			"name": "notepad++",
+			"down": "https://so.hacksafe.net/down/notepad.zip",
+			"size": 3984588,
+			"time": 1611048433
+		},
+		{
+			"name": "锐速加速",
+			"down": "https://so.hacksafe.net/down/ServerSpeeder.zip",
+			"size": 2097152,
+			"time": 1611048433
+		}
+		],
+		"description": "访问论坛出现问题的时候可以试试这个工具"
+	}]
+}
+);
